0d9548866041cf8558df6f104564f9cc658a1f0d
kimyundung <kimyundung@naver.com>
2021-07-29 19:29:26 +0800

    MyBatis CRUD

--- mybatis_quickstart/src/main/java/com/stone/test/MybatisTest.java
+++ mybatis_quickstart/src/main/java/com/stone/test/MybatisTest.java
@@ -27,7 +27,7 @@ public class MybatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         // 4. 执行sql : 参数 -> statementId -> namespace.id
-        List<User> userList = sqlSession.selectList("user.findAll");
+        List<User> userList = sqlSession.selectList("userMapper.findAll");
 
         // 5. 遍历打印
         for (User user : userList) {
@@ -35,6 +35,46 @@ public class MybatisTest {
         }
 
         // 6. 关闭资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSave() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user = new User(57L,"mybatis","mybatis@stone","北京","women","1");
+        int insert = sqlSession.insert("userMapper.saveUser", user);
+
+        sqlSession.commit(); //手动提交事务
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testUpdate() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user = new User(57L,"mybatisUpdate","mybatisUpdate@stone","北京Update","womenUpdate","1");
+        int update = sqlSession.update("userMapper.updateUser", user);
+
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        int delete = sqlSession.delete("userMapper.deleteUser", 57L);
+
+        sqlSession.commit();
         sqlSession.close();
     }
 }
